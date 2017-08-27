73afc6b4cec28c6aba14bc0852fa8dae005712ba
jasonqiangguo <jasonguo0509@gmail.com>
2017-08-27 00:09:09 -0400

    change

--- Muchlinski_growth_marginal.R
+++ Muchlinski_growth_marginal.R
@@ -73,7 +73,7 @@ set.seed(666) #the most metal seed for CV
 # model.btmchine <- bartMachine(X = data.full[,-1], y = factor(data.full$warstds), num_trees = 1000, num_burn_in = 2000, num_iterations_after_burn_in = 10000, serialize = T)
 # save(model.btmchine, file = "model.btmchine.RData")
 # 
-# load("model.btmchine.RData")
+load("model.btmchine.RData")
 
 # pdf("convergence_diagnostic.pdf", height = 6, width = 10)
 # plot_convergence_diagnostics(model.btmchine)
@@ -106,7 +106,7 @@ create_test_data <- function(choice){
   
   q <- apply(x[,1:length(covariates)], 2, median)
   test <- apply(matrix(q, nrow = 1), 2, function(x) rep(x, length(quant_prob)))
-  test[,which(names(covariates) == "trade")] <- quantile(covariates[,which(names(covariates) == "trade")], probs = quant_prob)
+  test[,which(names(covariates) == "gdpgrowth")] <- quantile(covariates[,which(names(covariates) == "gdpgrowth")], probs = quant_prob)
   test <- as.data.frame(test)
   names(test) <- names(covariates)
   return(test)
@@ -115,18 +115,18 @@ create_test_data <- function(choice){
 test <- lapply(c("high", "low"), create_test_data)
 
 
-# dbart_formula <- as.formula(paste0("data.full$warstds ~ ", paste(names(data.full)[-1], collapse = "+")))
-# model.dbart <- dbarts(dbartsData(dbart_formula, data.full, test = test[[1]]), control = dbartsControl(n.burn = 2000L, n.tree = 1000L, n.samples = 1000L, n.thin = 10))
-# dbart_result <- model.dbart$run()
-# save(dbart_result, file = "model.dbart.RData")
+dbart_formula <- as.formula(paste0("data.full$warstds ~ ", paste(names(data.full)[-1], collapse = "+")))
+model.dbart <- dbarts(dbartsData(dbart_formula, data.full, test = test[[1]]), control = dbartsControl(n.burn = 2000L, n.tree = 1000L, n.samples = 1000L, n.thin = 10))
+dbart_result <- model.dbart$run()
+save(dbart_result, file = "model.dbart.growth.RData")
 # 
 
-load("model.dbart.RData")
+load("model.dbart.growth.RData")
 
 dbart_phat <- apply(dbart_result$test, 1, function(x) mean(pnorm(x)))
 dbart.hpc <- apply(dbart_result$test, 1, function(x) quantile(pnorm(x), probs = c(0.05, 0.95)))
-dbart.plot.data <- as.data.frame(cbind(dbart_phat, dbart.hpc[1,], dbart.hpc[2,], test[[1]]$trade))
-names(dbart.plot.data) <- c("p_hat", "ci_lower_bd", "ci_upper_bd", "trade")
+dbart.plot.data <- as.data.frame(cbind(dbart_phat, dbart.hpc[1,], dbart.hpc[2,], test[[1]]$gdpgrowth))
+names(dbart.plot.data) <- c("p_hat", "ci_lower_bd", "ci_upper_bd", "gdpgrowth")
 
 # dbart.1.pred <- apply(dbart_result$train, 1, function(x) mean(pnorm(x)))
 # pred.dbart.1 <- prediction(dbart.1.pred, data.full$warstds)
@@ -152,72 +152,72 @@ marginal.mean.pred.high <- 1 - bart_machine_get_posterior(model.btmchine, new_da
 # calculate credible interval for each observation
 hpc_95_high <- 1 - calc_credible_intervals(model.btmchine, new_data = test[[1]])
 
-btmchine.plot.data <- as.data.frame(cbind(hpc_95_high[,1], hpc_95_high[,2], marginal.mean.pred.high, test[[1]]$trade))
-names(btmchine.plot.data) <- c("ci_upper_bd", "ci_lower_bd", "p_hat", "trade")
+btmchine.plot.data <- as.data.frame(cbind(hpc_95_high[,1], hpc_95_high[,2], marginal.mean.pred.high, test[[1]]$gdpgrowth))
+names(btmchine.plot.data) <- c("ci_upper_bd", "ci_lower_bd", "p_hat", "gdpgrowth")
 
-qqplot(btmchine.plot.data$p_hat, dbart.plot.data$p_hat, xlim = c(0,1), ylim = c(0, 1))
-abline(a = 0, b = 1, col = "red")
+# qqplot(btmchine.plot.data$p_hat, dbart.plot.data$p_hat, xlim = c(0,1), ylim = c(0, 1))
+# abline(a = 0, b = 1, col = "red")
 
 
-create_marginal_plot_trade <- function(df){
-  g <- ggplot(df, aes(x = trade, y = p_hat))
+create_marginal_plot_gdpgrowth <- function(df){
+  g <- ggplot(df, aes(x = gdpgrowth, y = p_hat))
   g <- g + geom_line() +  ylim(0, 1)
   g <- g + geom_errorbar(aes(ymax = ci_upper_bd, ymin = ci_lower_bd), width = 0.02)
   g <- g + theme_bw() + theme(panel.grid = element_blank(), plot.title = element_text(hjust = 0.5))
-  g <- g + xlab("Trade") + ylab("Probability")
+  g <- g + xlab("GDPgrowth") + ylab("Probability")
   g
 }
 
-btmchine.plot <- create_marginal_plot_trade(btmchine.plot.data)
-dbart.plot <- create_marginal_plot_trade(dbart.plot.data)
+btmchine.plot <- create_marginal_plot_gdpgrowth(btmchine.plot.data)
+dbart.plot <- create_marginal_plot_gdpgrowth(dbart.plot.data)
 
 
 
 
-model.probit<-glm(as.factor(warstds)~sxpnew+sxpsq+ln_gdpen+gdpgrowth+warhist+lmtnest+ef+popdense
-+lpopns+coldwar+seceduc+ptime+trade, 
-data=data.full, family=binomial(link = "probit"))
-
-model.probit
-
-
-
-create_test_data_probit <- function(choice){
-  if (choice == "high"){
-    covariates <- data.full[,c("sxpnew", "sxpsq", "ln_gdpen", "gdpgrowth", "warhist", "lmtnest", "ef", "popdense",
-                               "lpopns", "coldwar", "seceduc", "ptime", "trade")]
-    x <- covariates[which(data.full$warstds == 1),] 
-  }
-  if (choice == "low"){
-    covariates <- data.full[,c("sxpnew", "sxpsq", "ln_gdpen", "gdpgrowth", "warhist", "lmtnest", "ef", "popdense",
-                               "lpopns", "coldwar", "seceduc", "ptime", "trade")]
-    x <- covariates[which(data.full$warstds == 0),] 
-  }
-  
-  q <- apply(x[,1:length(covariates)], 2, median)
-  test <- apply(matrix(q, nrow = 1), 2, function(x) rep(x, length(quant_prob)))
-  test[,which(names(covariates) == "trade")] <- quantile(covariates[,which(names(covariates) == "trade")], probs = quant_prob)
-  test <- as.data.frame(test)
-  names(test) <- names(covariates)
-  return(test)
-}
-
-test <- lapply(c("high", "low"), create_test_data_probit)
-
-probit.pred <- predict(model.probit, newdata = test[[1]], type = "response", se.fit = TRUE)
-ci_probit_upper <- probit.pred$fit + 1.96*probit.pred$se.fit
-ci_probit_lower <- probit.pred$fit - 1.96*probit.pred$se.fit
-
-
-probit.plot.data <- as.data.frame(cbind(ci_probit_upper, ci_probit_lower, probit.pred$fit, test[[1]]$trade))
-names(probit.plot.data) <- c("ci_upper_bd", "ci_lower_bd", "p_hat", "trade")
-probit.plot <- create_marginal_plot_trade(probit.plot.data)
+# model.probit<-glm(as.factor(warstds)~sxpnew+sxpsq+ln_gdpen+gdpgrowth+warhist+lmtnest+ef+popdense
+# +lpopns+coldwar+seceduc+ptime+trade, 
+# data=data.full, family=binomial(link = "probit"))
+# 
+# model.probit
+# 
+# 
+# 
+# create_test_data_probit <- function(choice){
+#   if (choice == "high"){
+#     covariates <- data.full[,c("sxpnew", "sxpsq", "ln_gdpen", "gdpgrowth", "warhist", "lmtnest", "ef", "popdense",
+#                                "lpopns", "coldwar", "seceduc", "ptime", "trade")]
+#     x <- covariates[which(data.full$warstds == 1),] 
+#   }
+#   if (choice == "low"){
+#     covariates <- data.full[,c("sxpnew", "sxpsq", "ln_gdpen", "gdpgrowth", "warhist", "lmtnest", "ef", "popdense",
+#                                "lpopns", "coldwar", "seceduc", "ptime", "trade")]
+#     x <- covariates[which(data.full$warstds == 0),] 
+#   }
+#   
+#   q <- apply(x[,1:length(covariates)], 2, median)
+#   test <- apply(matrix(q, nrow = 1), 2, function(x) rep(x, length(quant_prob)))
+#   test[,which(names(covariates) == "trade")] <- quantile(covariates[,which(names(covariates) == "trade")], probs = quant_prob)
+#   test <- as.data.frame(test)
+#   names(test) <- names(covariates)
+#   return(test)
+# }
+# 
+# test <- lapply(c("high", "low"), create_test_data_probit)
+# 
+# probit.pred <- predict(model.probit, newdata = test[[1]], type = "response", se.fit = TRUE)
+# ci_probit_upper <- probit.pred$fit + 1.96*probit.pred$se.fit
+# ci_probit_lower <- probit.pred$fit - 1.96*probit.pred$se.fit
+# 
+# 
+# probit.plot.data <- as.data.frame(cbind(ci_probit_upper, ci_probit_lower, probit.pred$fit, test[[1]]$trade))
+# names(probit.plot.data) <- c("ci_upper_bd", "ci_lower_bd", "p_hat", "trade")
+# probit.plot <- create_marginal_plot_trade(probit.plot.data)
 
 
 pp <- grid.arrange(btmchine.plot, dbart.plot, probit.plot, ncol = 3)
-ggsave("btmachine_vs_dbart_marginal.pdf", pp, height = 8, width = 15)
+ggsave("btmachine_vs_dbart_marginal_gdpgrowth.pdf", pp, height = 8, width = 15)
 
-ggsave("probit_marginal.pdf", probit.plot, height = 6, width = 6)
+# ggsave("probit_marginal_gdpgrowth.pdf", probit.plot, height = 6, width = 6)
 
 #confusionMatrix(model.rf, norm="average")
 
