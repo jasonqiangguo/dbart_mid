1e7557cb18f07a7bbe01a89b4e6c79010e30693e
jasonqiangguo <jasonguo0509@gmail.com>
2017-08-27 23:02:16 -0400

    change

--- Muchlinski_growth_marginal.R
+++ Muchlinski_growth_marginal.R
@@ -95,18 +95,15 @@ test <- lapply(c("high", "low"), create_test_data)
 
 load(paste0(data.path, "/model.dbart.growth.RData"))
 
-dbarts.matrix.full <- dbart_result$test
 
 dbart_phat <- apply(dbart_result$test, 1, function(x) mean(pnorm(x)))
 dbart.hpc <- apply(dbart_result$test, 1, function(x) quantile(pnorm(x), probs = c(0.05, 0.95)))
 dbart.plot.data <- as.data.frame(cbind(dbart_phat, dbart.hpc[1,], dbart.hpc[2,], test[[1]]$gdpgrowth))
 names(dbart.plot.data) <- c("p_hat", "ci_lower_bd", "ci_upper_bd", "gdpgrowth")
 
-print(dim(dbart_result$test))
-                   
 dbart_posterior_draws <- as.data.frame(t(pnorm(dbart_result$test)))
 names(dbart_posterior_draws) <- unlist(lapply(seq(5, 95, by = 5), function(x) paste0("pct_", x)))
-write.dta(dbart_posterior_draws, file = paste0(script.path, "/dbart_posterior_draws.dta"))
+write.dta(dbart_posterior_draws, paste0(script.path, "/dbart_posterior_draws.dta"))
 
 # in bartMachine result the probability is reversed for 0 and 1, so when calculating the y_hat and confidence intervals we have to correct the problem using the yhat to subtract from 1
 btmachine.matrix.full <- 1 - bart_machine_get_posterior(model.btmchine, new_data = test[[1]])$y_hat_posterior_samples
@@ -118,7 +115,7 @@ names(btmchine.plot.data) <- c("p_hat", "ci_lower_bd", "ci_upper_bd", "gdpgrowth
 
 btmachine_posterior_draws <- as.data.frame(t(btmachine.matrix.full))
 names(btmachine_posterior_draws) <- unlist(lapply(seq(5, 95, by = 5), function(x) paste0("pct_", x)))
-write.dta(btmachine_posterior_draws, file = paste0(script.path, "/btmachine_posterior_draws.dta"))
+write.dta(btmachine_posterior_draws, paste0(script.path, "/btmachine_posterior_draws.dta"))
 
 
 create_marginal_plot_gdpgrowth <- function(df){
